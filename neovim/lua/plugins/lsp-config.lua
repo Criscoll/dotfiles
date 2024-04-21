ca2f33aa663db9c63ac3e0473747ce15bda3c69e
Cristian <cristiand2021@gmail.com>
2024-04-21 15:31:05 +1000

    21-04-2024 Update neovim dotfiles #2

--- neovim/lua/plugins/lsp-config.lua
+++ neovim/lua/plugins/lsp-config.lua
@@ -1,25 +1,80 @@
 return {
     { 
         'williamboman/mason.nvim',
+        dependencies = {
+          "williamboman/mason-lspconfig.nvim"
+        },
         config = function()
-            ui = {
+            -- import mason
+            local mason = require("mason")
+        
+            -- import mason-lspconfig
+            local mason_lspconfig = require("mason-lspconfig")
+        
+            -- enable mason and configure icons
+            mason.setup({
+              ui = {
                 icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
+                  package_installed = "✓",
+                  package_pending = "➜",
+                  package_uninstalled = "✗",
+                },
+              },
+            })
+        
+            mason_lspconfig.setup({
+              -- list of servers for mason to install
+              ensure_installed = {
+                "tsserver",
+                "html",
+                "cssls",
+                "tailwindcss",
+                "lua_ls",
+                "pyright",
+              },
+            })
         end
-
     },
-    { 
+    {
         'williamboman/mason-lspconfig.nvim',
-        config = function()
-            ensure_installed = { "lua_ls" }
-        end
     },
-    { 
-        'neovim/nvim-lspconfig'
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            -- import lspconfig plugin
+            local lspconfig = require("lspconfig")
+        
+            -- import mason_lspconfig plugin
+            local mason_lspconfig = require("mason-lspconfig")
+        
+            -- Change the Diagnostic symbols in the sign column (gutter)
+            local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+            for type, icon in pairs(signs) do
+              local hl = "DiagnosticSign" .. type
+              vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            end
+        
+            mason_lspconfig.setup_handlers({
+              -- default handler for installed servers
+              function(server_name)
+                lspconfig[server_name].setup({
+                })
+              end,
+              ["lua_ls"] = function()
+                -- configure lua server (with special settings)
+                lspconfig["lua_ls"].setup({
+                  settings = {
+                    Lua = {
+                      -- make the language server recognize "vim" global
+                      diagnostics = {
+                        globals = { "vim" },
+                      },
+                    },
+                  },
+                })
+              end,
+            })
+        end
     }
 }
 
