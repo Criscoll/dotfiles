26cd4f68570f6560a02f5ef7c4cbfe173593a235
Cristian <cristiand2021@gmail.com>
2024-06-10 15:43:36 +1000

    update neovim

--- neovim/nvim/lua/plugins/vim-fugitive.lua
+++ neovim/nvim/lua/plugins/vim-fugitive.lua
@@ -1,3 +1,6 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function ()
+        vim.api.nvim_set_keymap('n', '<Leader>gb', ':Git blame<CR>', {noremap = true, silent = true})
+    end
 }
