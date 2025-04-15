68c2245029f143ac3857c2ce4cf5cfc4264e14c0
Cristian <cristiand2021@gmail.com>
2025-04-15 18:30:26 +1000

    neovim evergarden theme

--- stow-managed/.config/nvim/lua/plugins/themes.lua
+++ stow-managed/.config/nvim/lua/plugins/themes.lua
@@ -43,6 +43,66 @@ return {
 --        end
     },
     {
+        'everviolet/nvim',
+        name = 'evergarden',
+        priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+        opts = {
+          theme = {
+            variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
+            accent = 'green',
+          },
+          editor = {
+            transparent_background = false,
+            sign = { color = 'none' },
+            float = {
+              color = 'mantle',
+              invert_border = false,
+            },
+            completion = {
+              color = 'surface0',
+            },
+          },
+        },
+        integrations = {
+          blink_cmp = true,
+          cmp = true,
+          fzf_lua = true,
+          gitsigns = true,
+          indent_blankline = { enable = true, scope_color = 'green' },
+          mini = {
+            enable = true,
+            animate = true,
+            clue = true,
+            completion = true,
+            cursorword = true,
+            deps = true,
+            diff = true,
+            files = true,
+            hipatterns = true,
+            icons = true,
+            indentscope = true,
+            jump = true,
+            jump2d = true,
+            map = true,
+            notify = true,
+            operators = true,
+            pick = true,
+            starters = true,
+            statusline = true,
+            surround = true,
+            tabline = true,
+            test = true,
+            trailspace = true,
+          },
+          nvimtree = true,
+          rainbow_delimiters = true,
+          symbols_outline = true,
+          telescope = true,
+          which_key = true,
+          neotree = true,
+        },
+    },
+    {
         'savq/melange-nvim',
         lazy = false,
         priority = 1000,
